fee4ba7cf932ee4d7a3a9202741f92db5f223db0
21cookej <21cookej@roundwoodpark.co.uk>
2024-07-08 15:02:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -329,7 +329,7 @@ block4.prototype.act = function (step) {
 
 function Player(pos) {
   this.pos = pos.plus(new Vector(0, -0.4));
-  this.size = new Vector(0.95, 0.95);
+  this.size = new Vector(0.85, 0.85);
   this.speed = new Vector(0, 0);
 }
 Player.prototype.type = "player";
