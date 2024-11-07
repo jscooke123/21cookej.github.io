c5aaef5f0151f072b44028f771d07f30a08b121e
21cookej <21cookej@roundwoodpark.co.uk>
2024-11-07 10:39:14 +0000

    Update infinite.js

--- infinite/infinite.js
+++ infinite/infinite.js
@@ -1,1 +1,1 @@
-javascript:alert("Are You Sure?");alert("Use At Your Own Risk");alert("Ready? IF NO CLICK OK NOW.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
+javascript:alert("Ready? IF NO CLICK OK NOW.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
