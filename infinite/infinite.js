bd44c141a7240fb9989a9347bd4c2bf2eb39a749
roundwood-park <jscooke123@outlook.com>
2024-11-25 11:17:06 +0000

    Update infinite.js

--- infinite/infinite.js
+++ infinite/infinite.js
@@ -1,2 +1,2 @@
 // javascript:alert("Ready? IF NO CLICK OK.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
-javascript:setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
+javascript:setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000000);
